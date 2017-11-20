70e36d23ab89739f39ecf40cca78445ace2a08b6
Andrés Muñoz <andy_mc@Andress-MacBook-Pro.local>
2017-11-20 16:34:23 -0500

    end mvp front-end

--- client/app/scripts/group/controller.js
+++ client/app/scripts/group/controller.js
@@ -1,15 +1,68 @@
 'use strict';
 
 angular.module('Group')
-.controller('group', function ($scope) {
-
+.controller('group', function ($scope, _, data) {
   $scope.controller_loaded = 'Group loaded!';
 
+  $scope.parse_csv = function() {
+    var employees_ids = Papa.parse(data.data, {delimiter: ','}).data;
+    return _(employees_ids).map(function(couple){
+      return [parseInt(couple[0]), parseInt(couple[1])];
+    });
+  };
+
+  $scope.get_pairs = function (employees_list) {
+    var employees_ids = _(employees_list)
+      .chain()
+      .flatten()
+      .uniq()
+      .value();
+
+    var employees_frequency = _(employees_ids).map(function(id) {
+      var frequency = employees_list.filter(function(couple) {
+        return (couple.indexOf(id) !== -1);
+      }).length;
+      return [id, frequency];
+    });
+
+    var order_employees_freq = _(employees_frequency).sortBy(function(employee){
+      return employee[1];
+    });
+
+    var winners = [];
+    while (employees_list.length > 0) {
+      var user = order_employees_freq.pop();
+      if (user[1] > 1) {
+        employees_list = _(employees_list).filter(function(couple){
+          return (couple.indexOf(user[0]) === -1);
+        });
+        winners.push(user[0]);
+      } else {
+        _(employees_list).each(function(employee, index){
+          if (index % 2 === 0) {
+            winners.push(employee[0]);
+          } else {
+            winners.push(employee[1]);
+          }
+        });
+        break;
+      }
+    }
+    
+    winners.sort();
+
+    $scope.result = winners;
+  };
 })
 .config(function ($routeProvider) {
   $routeProvider
   .when('/group', {
     templateUrl: 'scripts/group/views/group.html',
-    controller: 'group'
+    controller: 'group', 
+    resolve: {
+      data: function($http) {
+        return $http.get('./scripts/group/specs/test_employees.csv');
+      }
+    }
   });
 });
--- client/app/scripts/group/specs/controller_spec.js
+++ client/app/scripts/group/specs/controller_spec.js
@@ -1,15 +1,18 @@
 'use strict';
 
 describe('Controller: select group', function () {
-
   beforeEach(module('Group'));
 
   var controller;
   var scope;
+  var data = {data: '1009, 2011\n1017, 2011' };
 
   beforeEach(inject(function ($rootScope, $controller) {
     scope = $rootScope.$new();
-    controller = $controller('group', { $scope: scope });
+    controller = $controller('group', {
+      $scope: scope,
+      data: data
+    });
   }));
 
   describe('On instance', function () {
@@ -18,17 +21,69 @@ describe('Controller: select group', function () {
     });
   });
 
+  describe('When parsing csv', function() {
+    it('should read the csv file', function() {
+      
+    });
+
+    it('should convert csv file to an 2D array', function () {
+      expect(scope.parse_csv()).toEqual([
+        [1009, 2011],
+        [1017, 2011]
+      ]);
+    });
+  });
+
+  describe('when get result', function () {
+    it('should return case 1', function(){
+      var input = [
+        [1009, 2011],
+        [1017, 2011]
+      ];
+
+      scope.get_pairs(input);
+      expect(scope.result).toEqual([2011]);
+    });
+
+    it('should return case 2', function(){
+      var input = [
+        [1009, 2000],
+        [1009, 2001],
+        [1002, 2002],
+        [1003, 2002]
+      ];
+
+      scope.get_pairs(input);
+      expect(scope.result).toEqual([1009, 2002]);
+    });
+
+    it('should return case 2', function(){
+      var input = [
+        [1009, 2000],
+        [1009, 2001],
+        [1001, 2002],
+        [1002, 2003]
+      ];
+
+      scope.get_pairs(input);
+      expect(scope.result).toEqual([1001, 1009, 2003]);
+    });
+  });
+
   describe('when going to /group', function () {
 
     var route, location, rootScope, httpBackend;
+    var http;
 
-    beforeEach(inject(function ($route, $location, $rootScope, $httpBackend) {
+    beforeEach(inject(function ($route, $location, $rootScope, $httpBackend, $http) {
       route = $route;
       location = $location;
       rootScope = $rootScope;
       httpBackend = $httpBackend;
-
+      http = $http;
+    
       httpBackend.when('GET', 'scripts/group/views/group.html').respond('<div></div>');
+      httpBackend.when('GET', './scripts/group/specs/test_employees.csv').respond('');
     }));
 
     afterEach(function () {
@@ -45,6 +100,15 @@ describe('Controller: select group', function () {
 
       expect(route.current.templateUrl).toBe('scripts/group/views/group.html');
       expect(route.current.controller).toBe('group');
+    });
+
+    it('should read test_employees.csv file correctly', function() {
+      spyOn(http, 'get').and.returnValue('data_csv');
+
+      var result = route.routes['/group'].resolve.data(http);
+
+      expect(http.get).toHaveBeenCalledWith('./scripts/group/specs/test_employees.csv'); 
+      expect(result).toBe('data_csv');
     });
   });
 
